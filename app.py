2c0708e0f889fcebd33a6664a8358420805da12e
Ferco35 <79657934+Ferco35@users.noreply.github.com>
2022-11-12 13:05:47 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ nbrTopics=st.number_input('Nombre de topics')
 if st.button('Detect'):
     st.write(text)
     st.write(nbrTopics)
-    precision,topics=predictions.predict(text,nbrTopics)
+    precision,topics=predictions.predict(text,int(nbrTopics))
     if(precision<0):
         st.alert("Polarité : ", precision ," - COMMENTAIRE NEGATIF")
         st.info(topics)
